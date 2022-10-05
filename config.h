977dac8426a025c807f3ac096638180169b097d1
StepanovPlaton <mail2platon@yandex.ru>
2022-10-05 14:53:31 +0400

    05/10 Add menu-qalc

--- config.h
+++ config.h
@@ -13,22 +13,22 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 
-static const int vertpad            = 8;       /* vertical padding of bar */
-static const int sidepad            = 6;       /* horizontal padding of bar */
-static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 5;       /* horizontal padding of bar */
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 10;        /* vertical padding for statusbar */
 
 static const int statusvertpadbar   = 4;
 static const int systrayvertpad     = 2;
 
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
 static const char *fonts[]          = { "Liberation Sans:style=Bold:size=10:antialias=true:autohint=true", "Font Awesome 6 Free Solid:style=Solid:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=12:antialias=true:autohint=true";
+static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=16:antialias=true:autohint=true";
 
 static const char col_gray[]        = "#151515";//"#3b3b3b";
 static const char col_white[]       = "#cccccc";//"#ffffff";
@@ -78,7 +78,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-x", "8", "-y", "10", "-z", "1280", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray, "-nf", col_white, "-sb", col_gray, "-sf", col_yellow, NULL };
+// dmenu_run -x 5 -y 5 -z 1270 -m 0 -fn 'CodeNewRoman Nerd Font Mono:size=16:antialias=true:autohint=true' -nb '#151515' -nf '#cccccc' -sb '#151515' -sf '#e7c547'
+static const char *dmenucmd[] = { "dmenu_run", "-x", "5", "-y", "5", "-z", "1270", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray, "-nf", col_white, "-sb", col_gray, "-sf", col_yellow, NULL };
 
 static const char *termcmd[]  = { "st", NULL };
 
---
+++ dmenu/menu-qalc/README.md
@@ -0,0 +1,46 @@
+# menu-qalc
+A calculator for Rofi/dmenu(2)
+
+[Screencast](https://gfycat.com/SociableDopeyHerald)
+
+## Installation
+Arch Linux AUR: [menu-qalc](https://aur.archlinux.org/packages/menu-qalc/)
+
+## Dependencies
+- [libqalculate](https://archlinux.org/packages/extra/x86_64/libqalculate/)
+- [xclip](https://www.archlinux.org/packages/extra/x86_64/xclip/)
+- [Rofi](https://aur.archlinux.org/packages/rofi-git/) or
+  dmenu[(2)](https://aur.archlinux.org/packages/dmenu2/)
+
+## Usage
+`menu-qalc` uses `qalc` as the backend and will accept any operations `qalc` is able to do:
+
+    = 4+4
+    = (4+2)/(4+3)
+    = 4^2
+    = sqrt(4)
+    = c(2)
+    = '500 + 25%'
+    = '1000 EUR to USD'
+
+The answer can be copied to the clipboard and used for further calculations
+inside (or outside) Rofi/dmenu.
+
+If launched outside of Rofi/dmenu the expression may need quotation marks.
+
+## Custom Usage
+To launch directly into the calculator, use the following command (useful if
+bound to "super + equal" in [sxhkd](https://github.com/baskerville/sxhkd),
+[i3](https://i3wm.org/) or the like):
+
+    = -- [rofi/dmenu parameters]
+
+For example:
+
+    = -- -location 2 -width 100
+
+### Force usage of `dmenu`
+By default, if `rofi` is installed, it will be used. You can force `menu-qalc`
+to use `dmenu` or any other `dmenu`-like application:
+
+    = --dmenu=dmenu
